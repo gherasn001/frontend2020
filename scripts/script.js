4c303cfd39b9837f3a4bf60beabde121c95c5a4c
gherasn001 <32016426+gherasn001@users.noreply.github.com>
2020-09-27 20:03:56 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,20 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+
 // JavaScript Document
-var hamburgerButton = document.querySelector('header > button')
-var hamburgerNav = document.querySelector('header > nav')
+var hamburgerButton = document.querySelector('header > button');
+var hamburgerNav = document.querySelector('header > nav');
+
 
 var open = function menuOpen() {
 
     hamburgerNav.classList.toggle('menu');
-}
+
+};
 
 hamburgerButton.addEventListener('click', open);
+
+
 
 //var mouseHover = document.querySelector('main > section:nth-of-type(1) > li > img')
 //var hoverLink = document.querySelector('main > section:nth-of-type(1) > li:nth-of-type(1) > a')
